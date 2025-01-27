8cb51bf61a7238b7b98450fd1240d5ac2f3df2c8
Tom Schmidt <schmidttom@comcast.net>
2025-01-27 10:48:55 -0700

    Fixed builder arch selection

--- buildAddOn.sh
+++ buildAddOn.sh
@@ -5,7 +5,7 @@ PATHTOBUILD="$BUILDCONTAINER_DATA_PATH"
 BUILDERARCH=amd64
 
 #ARCH=all
-#ARCH=amd64
+ARCH=amd64
 #ARCH=aarch64
 #ARCH=aarmv7
 
@@ -13,12 +13,12 @@ echo "project directory is $PROJECTDIR"
 echo "build container data path is $BUILDCONTAINER_DATA_PATH"
 echo "build container target build path is $PATHTOBUILD"
 #Build remote
-CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro ghcr.io/home-assistant/{BUILDERARCH}-builder:latest --$ARCH -t spotify -r https://github.com/nedguy/addon-spotify-connect -b main"
+CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro ghcr.io/home-assistant/$BUILDERARCH-builder:latest --$ARCH -t spotify -r https://github.com/nedguy/addon-spotify-connect -b main"
 
 #Build local
-#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ./spotify:$BUILDCONTAINER_DATA_PATH ghcr.io/home-assistant/{BUILDERARCH}-builder:latest --target $PATHTOBUILD --$ARCH"
+#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ./spotify:$BUILDCONTAINER_DATA_PATH ghcr.io/home-assistant/$BUILDERARCH-builder:latest --target $PATHTOBUILD --$ARCH"
 
 #Build and deploy local
-#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ./spotify:$BUILDCONTAINER_DATA_PATH ghcr.io/home-assistant/{BUILDERARCH}-builder:latest --target $PATHTOBUILD --$ARCH --test --docker-hub local"
+#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ./spotify:$BUILDCONTAINER_DATA_PATH ghcr.io/home-assistant/$BUILDERARCH-builder:latest --target $PATHTOBUILD --$ARCH --test --docker-hub local"
 echo "$CMD"
 $CMD

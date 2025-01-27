fad83998b59de8f20595c77bb070e83618a0a2f2
Tom Schmidt <schmidttom@comcast.net>
2025-01-27 10:37:00 -0700

    Updating to librespot v0.6.0 and 17.1.0 base

---
+++ buildAddOn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BUILDCONTAINER_DATA_PATH="/data"
+PATHTOBUILD="$BUILDCONTAINER_DATA_PATH"
+#ARCH=all
+#ARCH=amd64
+ARCH=aarch64
+#ARCH=aarmv7
+
+echo "project directory is $PROJECTDIR"
+echo "build container data path is $BUILDCONTAINER_DATA_PATH"
+echo "build container target build path is $PATHTOBUILD"
+#Build on apple silicone
+CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro ghcr.io/home-assistant/aarch64-builder:latest --$ARCH -t spotify -r https://github.com/nedguy/addon-spotify-connect -b main"
+
+#Build on intel silicone
+#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro ghcr.io/home-assistant/amd64-builder:latest --$ARCH -t spotify -r https://github.com/nedguy/addon-spotify-connect -b main"
+
+#Build local
+#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ./spotify:$BUILDCONTAINER_DATA_PATH ghcr.io/home-assistant/amd64-builder:latest --target $PATHTOBUILD --$ARCH "
+
+#Build and deploy local
+#CMD="docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock:ro -v ./spotify:$BUILDCONTAINER_DATA_PATH ghcr.io/home-assistant/amd64-builder:latest --target $PATHTOBUILD --$ARCH --test --docker-hub local"
+echo "$CMD"
+$CMD
